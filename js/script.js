b33681cd2a6887df9685a05baaa2fcbe7b559c9e
Andrea-Baggio <andreabaggio800@gmail.com>
2022-10-25 14:03:39 +0200

    finito

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
-/*
-Consegna:
-Scrivi un programma che stampi in console i numeri da 1 a 100,
-ma che per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi “Buzz”.
-Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
-*/
 
-for (let i = 0; i <= 100; i++) {
-	console.log(i);
+const eleSquares = document.querySelector('.squares');
 
-    if (i % 5 == 0) {
-        console.log('multiplo di 5');
-    } else if (i % 3 == 0) {
-        console.log('multiplo di 3');
+
+for (i = 1; i <= 100; i++) {
+    let iString = i;
+
+    if (i % 15 == 0) {
+        iString = 'FizzBuzz';
+        eleSquares.innerHTML += `<div class= "sq fizzbuzz">${iString}</div>`;
+    } else if (i % 5 == 0){
+        iString = 'Fizz';
+        eleSquares.innerHTML += `<div class= "sq fizz">${iString}</div>`;
+    } else if (i % 3 == 0){
+        iString = 'Buzz';
+        eleSquares.innerHTML += `<div class= "sq buzz">${iString}</div>`;
+    } else {
+        eleSquares.innerHTML += `<div class="sq">${iString}</div>`;
     }
+    console.log(iString);   
 }
